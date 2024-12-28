e31882f7f0e037ea88fa8380a2c707c03af03dae
shraddhahiremath114 <shraddhahiremath114@gmail.com>
2024-12-28 21:24:43 +0530

    MoveZeros

---
+++ ds150/Arrays/MoveZeros.java
@@ -0,0 +1,19 @@
+public class MoveZeros{
+    public static void main(String[] args) {
+        int[] arr={1,0,2,0,3,0,4,0,5,0};
+        int n=arr.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                arr[count++]=arr[i];
+
+            }
+        }
+        while(count<n){
+            arr[count++]=0;
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
