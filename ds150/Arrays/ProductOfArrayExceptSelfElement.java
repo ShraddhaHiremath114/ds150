1f0ee1bc277ea5ce888d83b8a783ae9ecf41a23f
vireshh22 <viresh0489@gmail.com>
2024-12-29 11:12:20 +0530

    ProductofArrExceptself

--- ds150/Arrays/ProductOfArrayExceptSelfElement.java
+++ ds150/Arrays/ProductOfArrayExceptSelfElement.java
@@ -1,4 +1,22 @@
 public class ProductOfArrayExceptSelfElement {
+    public static void method2(int[] arr){
+        int n=arr.length;
+        int[] prefix=new int[n];
+        int[] suffix=new int[n];
+
+        prefix[0]=1;
+        suffix[n-1]=1;
+
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]*arr[i-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*arr[i+1];
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(prefix[i]*suffix[i]+" ");
+        }
+    }
     public static void main(String[] args) {
         int[] arr={1,2,3,4};
         
@@ -17,5 +35,7 @@ public class ProductOfArrayExceptSelfElement {
         for(int i=0;i<n;i++){
             System.out.print(ans[i]+" ");
         }
+        System.out.println();
+        method2(arr);
     }
 }
